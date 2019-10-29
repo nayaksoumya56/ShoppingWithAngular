2e55fef8a138b3e3c19aa8b4730f277641f5e4ee
soumya <soumya@divami.com>
2019-10-29 16:03:28 +0530

    `

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListsComponent } from './recipes/recipes-lists/recipes-lists.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes-list/recipes-item/recipes-item.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -18,9 +16,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListsComponent,
     RecipesDetailComponent,
-    RecipesItemComponent,
     RecipesListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
