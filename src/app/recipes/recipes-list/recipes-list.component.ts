4b9c58e5ff18a6e07fb384a81c472d1ba5965121
soumya <soumya@divami.com>
2019-10-31 15:01:20 +0530

    added recipes to recipe list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Recipe', 'This is a Desciption','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Recipe', 'This is a Desciption','http://c1.peakpx.com/wallpaper/400/456/943/dishes-kitchen-bio-food-recipe-wallpaper-preview.jpg'),
+    new Recipe('Recipe', 'This is a Desciption','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Recipe', 'This is a Desciption','http://c1.peakpx.com/wallpaper/400/456/943/dishes-kitchen-bio-food-recipe-wallpaper-preview.jpg')
+  ];
 
   constructor() { }
 
